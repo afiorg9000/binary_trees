369c763d6e868748e5eefff84db4645966655b00
afiorg9000 <3533@holbertonschool.com>
2022-03-22 19:06:18 -0400

    Insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = binary_tree_node(parent, value);
+
+    if (parent == NULL || node == NULL)
+    {
+        return (NULL);
+    }
+    if (parent->left != NULL)
+    {
+        node->left = parent->left;
+        parent->left->parent = node;
+    }
+    else
+    parent->left = node;
+
+    return (node);
+}
