89a9e4f9dcbb7f0c55f934544d4d928d1f0b827f
afiorg9000 <3533@holbertonschool.com>
2022-03-22 19:07:01 -0400

    Insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = binary_tree_node(parent, value);
+
+    if (parent == NULL || node == NULL)
+    {
+        return (NULL);
+    }
+
+    if (parent->right != NULL)
+    {
+        node->right = parent->right;
+        parent->right->parent = node;
+    }
+    parent->right = node;
+
+    return (node);
+}
